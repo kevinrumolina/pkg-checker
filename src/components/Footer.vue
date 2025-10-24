6dcefab554a6a4a96f752a31eff297248c88ede5
Kevin Jair Ruiz Molina <kerumo@Valerias-MacBook-Pro.local>
2025-10-23 21:55:02 -0400

    update: replace scaffold vue files with new header & footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer>
+        <p>For more info about the API consult <a src="https://osv.dev/" target="_blank">OSV API</a></p>
+        <p>©2025 <a href="https://kerumosolutions.com" target="_blank">Kevin Ruiz Molina</a>. All rights reserved.</p>
+    </footer>
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+    footer {
+        position: fixed;
+        bottom: 0;
+        padding: 16px;
+        display: flex;
+        justify-content: center;
+        width: 100vw;
+        flex-wrap: wrap;
+        background-color: var(--gray);
+    }
+    a {
+        font-weight: 700;
+        color: #FFF;
+    }
+    @media screen and (min-width: 768px) {
+        footer { 
+            justify-content: space-between;
+        }
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <img src="https://google.github.io/osv.dev/assets/logo-dark.png" alt="OSV Logo">
+        <h1>Vulnerabilities Scanner</h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 50px;
+    }
+    h1 {
+        font-size: 36px;
+        margin-top: 25px;
+        text-align: center;
+    }
+    img {
+        padding: 0 20%;
+        width: 100%;
+    }
+    @media screen and (min-width: 768px) {
+        h1 { 
+            font-size: 64px;
+        }
+    }
+</style>
